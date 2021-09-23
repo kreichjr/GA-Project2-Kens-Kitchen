7d7dd2eb97aa929bf816941a236d429b403aaef8
kreichjr <kreichjr@gmail.com>
2021-09-22 21:25:27 -0500

    Completed create route for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt')
 const User = require('../models/users')
 
 // Express Routes
-router.get("/register", (req, res) => {
+router.get('/register', (req, res) => {
 	res.render('users/register.ejs')
 })
 
@@ -19,5 +19,32 @@ router.get('/setMeAsAdmin', (req, res) => {
 		})
 	}
 })
+
+router.post('/register', (req, res) => {
+	if (req.body.password !== req.body.verifyPassword) {
+		req.session.message = 'Password and Verify Password do not match'
+		res.redirect('/users/register')
+		return
+	}
+	const salt = bcrypt.genSaltSync(10)
+	req.body.password = bcrypt.hashSync(req.body.password, salt)
+
+	User.findOne({username: req.body.username}, (err, userExists) => {
+		if (err) return res.send(err)
+		if (userExists) {
+			req.session.message = 'User already exists'
+			res.redirect('/users/register')
+			return
+		}
+		delete req.body.verifyPassword
+		User.create(req.body, (err, createdUser) => {
+			if (err) return res.send(err)
+			req.session.currentUser = createdUser
+			res.redirect('/ingredients')
+		})
+	})
+})
+
+
 
 module.exports = router
