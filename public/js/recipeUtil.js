c971d6bda4d0c0441af0c219a9f5807401bd3c88
kreichjr <kreichjr@gmail.com>
2021-10-01 19:04:39 -0500

    Added ingredient selector on recipe new form as well as come js code

--- public/js/recipeUtil.js
+++ public/js/recipeUtil.js
@@ -1,6 +1,14 @@
 const addBtn = document.querySelector('#add-ingredient-btn')
-const ingredientSelector = document.querySelector('#ingredient-selector-input')
+const ingredientSelector = document.querySelector('#ingredient-selector-0')
+
 
 addBtn.addEventListener('click', (event) => {
+	if (!ingredientSelector?.value) return
+	
 	console.log(ingredientSelector.value)
+	let newOption = document.createElement('option')
+	newOption.value = ingredientSelector.value
+	newOption.innerText = ingredientSelector.value
+	ingredientSelectorDisplay.append(newOption)
+
 })
